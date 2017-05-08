42f85f1a004acc9c9e1633391579fb29dae00471
helenebm <helene.midtfjord@outlook.com>
2017-05-08 20:19:48 +0200

    Add files via upload

--- spatiotemporal_summation_curve_edited.py
+++ spatiotemporal_summation_curve_edited.py
@@ -7,35 +7,35 @@ import pylgn.kernels.spatial as spl
 import pylgn.kernels.temporal as tpl
 
 # fb weights:
-fb_weights = [0, 0.6]
+fb_weights = [0.6, 0.9]
 
 # diameters
-mask_size = np.linspace(0, 10, 40) * pq.deg
+mask_size = np.linspace(0, 6, 40) * pq.deg
 
 # list to store spatiotemporal summation curves for each weight
 responses = []
 
-for w_c in fb_weights:
+for w_fb in fb_weights:
 
     # create network
     network = pylgn.Network()
 
     # create integrator
-    integrator = network.create_integrator(nt=10, nr=7, dt=1*pq.ms, dr=1*pq.deg)
+    integrator = network.create_integrator(nt=10, nr=7, dt=1*pq.ms, dr=0.1*pq.deg)
     
     # create spatial kernels
-    Wg_s = spl.create_dog_ft(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg)      # Stimuli   -> Ganglion, (edog-paper)
-    Krg_s = spl.create_gauss_ft(A=1, a=0.1*pq.deg)                           # Gangllion -> Relay     (honda-paper)
-    Kcr_s = spl.create_delta_ft()                                            # Relay     -> Cortical_cen_sur 
-    Krcr_cen_s = spl.create_gauss_ft(A=2, a=(0.1)*pq.deg)                    # Cortical  -> Relay_cen
-    Krcr_sur_s = spl.create_gauss_ft(A=1, a=(0.9)*pq.deg)                    # Cortical  -> Relay_sur
+    Wg_s = spl.create_dog_ft(A=1, a=0.62*pq.deg, B=0.85, b=1.26*pq.deg)
+    Krg_s = spl.create_gauss_ft(A=1, a=0.1*pq.deg)  
+    Kcr_s = spl.create_delta_ft()
+    Krcr_cen_s = spl.create_gauss_ft(A=1, a=(0.1)*pq.deg)
+    Krcr_sur_s = spl.create_gauss_ft(A=2, a=(0.9)*pq.deg)
     
     # create temporal kernels
-    Wg_t = tpl.create_biphasic_ft(phase_duration =42.5*pq.ms, damping_factor =0.38, delay =0 *pq.ms) # param. values from Norheim, Wyller, Einevoll                                       # Stimnuli -> Ganglion
-    Krg_t = tpl.create_exp_decay_ft(1 *pq.ms, delay = 1 *pq.ms)              # Ganglion      -> Relay
-    Kcr_t = tpl.create_delta_ft()                                            # Relay         -> Cortical_cen_sur
-    Krcr_cen_t = tpl.create_exp_decay_ft(1 *pq.ms, delay = 2 *pq.ms)        # Cortical_cen  -> Relay
-    Krcr_sur_t = tpl.create_exp_decay_ft(1 *pq.ms, delay = 20 *pq.ms)         # Cortical_sur  -> Relay
+    Wg_t = tpl.create_biphasic_ft(phase_duration =42.5*pq.ms, damping_factor =0.38, delay =0 *pq.ms) 
+    Krg_t = tpl.create_exp_decay_ft(18 *pq.ms, delay = 0 *pq.ms) 
+    Kcr_t = tpl.create_delta_ft()     
+    Krcr_cen_t = tpl.create_exp_decay_ft(1 *pq.ms, delay = 0 *pq.ms)  # decay: 10 ms, delay: 30 ms
+    Krcr_sur_t = tpl.create_exp_decay_ft(1 *pq.ms, delay = 30 *pq.ms) # decay: 20 ms, delay: 30 ms
     
     # create neurons
     ganglion = network.create_ganglion_cell(kernel=(Wg_s, Wg_t))
@@ -44,17 +44,18 @@ for w_c in fb_weights:
     cortical_sur = network.create_cortical_cell()
                     
     # connect neurons
-    network.connect(ganglion, relay, (Krg_s, Krg_t), 1.0)                   # Ganglion      -> Relay
-    network.connect(cortical_cen, relay, (Krcr_cen_s, Krcr_cen_t), w_c)     # Cortical_cen  -> Relay
-    network.connect(cortical_sur, relay, (Krcr_sur_s, Krcr_sur_t), -w_c)     # Cortical_sur  -> Relay
-    network.connect(relay, cortical_cen, (Kcr_s, Kcr_t), 1.0)               # Relay         -> Cortical_cen
-    network.connect(relay, cortical_sur, (Kcr_s, Kcr_t), 1.0)               # Relay         -> Cortical_sur
+    network.connect(ganglion, relay, (Krg_s, Krg_t), 1.0)  
+    network.connect(cortical_cen, relay, (Krcr_cen_s, Krcr_cen_t), w_fb)
+    network.connect(cortical_sur, relay, (Krcr_sur_s, Krcr_sur_t), -w_fb) 
+    network.connect(relay, cortical_cen, (Kcr_s, Kcr_t), 1.0)       
+    network.connect(relay, cortical_sur, (Kcr_s, Kcr_t), 1.0)    
+    
 
     st_summation_curve = np.zeros([len(mask_size), integrator.Nt]) / pq.s
     for i, d in enumerate(mask_size):
         # create stimulus
-        k_pg = integrator.spatial_freqs[3]                                  # spatial_freqs[0] -> flashing splot
-        w_pg = integrator.temporal_freqs[1]                                 # temporal_freqs[0] -> static stimulus 
+        k_pg = integrator.spatial_freqs[3]  # 0 for flashing spot                         
+        w_pg = integrator.temporal_freqs[1] # 0 for static stimuli
         stimulus = pylgn.stimulus.create_patch_grating_ft(wavenumber=k_pg,
                                                           angular_freq=w_pg,
                                                           mask_size=d)
@@ -72,18 +73,16 @@ fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), sharey="row")
 
 # xmin, xmax, ymin, ymax:
 extent = [integrator.times.min(), integrator.times.max(), mask_size.min(), mask_size.max()]
-#vmin = -0.6
-#vmax = 0.6
+vmin = -10
+vmax = 10
 
-im1 = ax1.imshow(responses[0], extent=extent, origin="lower", aspect="auto",
-                 vmin=vmin, vmax=vmax)
+im1 = ax1.imshow(responses[0], extent=extent, origin="lower", aspect="auto", vmin=vmin, vmax=vmax, cmap="Blues_r")
 ax1.set_title("Response (FB weight={})".format(fb_weights[0]))
 ax1.set_ylabel("Patch size (deg)")
 ax1.set_xlabel("Time (ms)")
 plt.colorbar(im1, ax=ax1)
 
-im2 = ax2.imshow(responses[1], extent=extent, origin="lower", aspect="auto",
-                 vmin=vmin, vmax=vmax)
+im2 = ax2.imshow(responses[1], extent=extent, origin="lower", aspect="auto", vmin=vmin, vmax=vmax, cmap="Blues_r")
 ax2.set_title("Resoinse (FB weight={})".format(fb_weights[1]))
 ax2.set_xlabel("Time (ms)")
 plt.colorbar(im2, ax=ax2)
